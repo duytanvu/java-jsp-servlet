6c8a28362f6fec1968240934b3460f8be8c830fa
Tan Vu <duytanvu@icloud.com>
2021-01-12 09:56:36 -0500

    Add CollegeInterface.java

---
+++ src/webd4021/vud/CollegeInterface.java
@@ -0,0 +1,8 @@
+package webd4021.vud;
+
+public interface CollegeInterface {
+    final String COLLEGE_NAME = "Durham College";
+    final String PHONE_NUMBER = "(905)721-2000";
+
+    public String getTypeForDisplay();
+}
